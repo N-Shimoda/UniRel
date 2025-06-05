310ba1f57291ddc3b2c1434bc851f0a39955f7cb
N-Shimoda <naoki.shimoda@outlook.jp>
2025-06-05 20:50:39 +0900

    removed empty line

--- model/modify_bert.py
+++ model/modify_bert.py
@@ -977,7 +977,6 @@ BERT_INPUTS_DOCSTRING = r"""
 )
 class BertModel(BertPreTrainedModel):
     """
-
     The model can behave as an encoder (with only self-attention) as well as a decoder, in which case a layer of
     cross-attention is added between the self-attention layers, following the architecture described in `Attention is
     all you need <https://arxiv.org/abs/1706.03762>`__ by Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit,
